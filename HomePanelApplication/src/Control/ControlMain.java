0c1c450f10d190487e02a9a1769a450274a7a236
Kim YoungMin <happistar09@naver.com>
2017-04-26 17:58:38 +0900

    HomePanel -1

--- HomePanelApplication/src/Control/ControlMain.java
+++ HomePanelApplication/src/Control/ControlMain.java
@@ -11,7 +11,7 @@ public class ControlMain extends Application{
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		Parent parent = FXMLLoader.load(getClass().getResource("control.fxml"));      
+		Parent parent = FXMLLoader.load(getClass().getResource("MainControl.fxml"));      
 		Scene scene = new Scene(parent);
 		primaryStage.setScene(scene);
 		primaryStage.setTitle("Control");
