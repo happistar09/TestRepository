ca68706c8fd3a2de385fe58386c0431408363ec1
Kim YoungMin <happistar09@naver.com>
2017-06-15 17:56:08 +0900

    6/15

--- Network/src/http/Exam01.java
+++ Network/src/http/Exam01.java
@@ -1,37 +1,52 @@
 package http;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import org.apache.http.HttpEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
+import org.json.JSONObject;
 
 public class Exam01 {
-	//Field
-	
-	
-	//Constructor
-	
-	//Method
+
 	public static void main(String[] args) throws IOException {
 		CloseableHttpClient httpclient = HttpClients.createDefault();
-		HttpGet httpget = new HttpGet("http://192.168.3.24:8080/iot/temperature");
-		CloseableHttpResponse response = httpclient.execute(httpget);
 		try {
-			HttpEntity entity = response.getEntity();
-			if (entity != null) {
-				InputStream instream = entity.getContent();
-				try {
-					// do something useful
-					System.out.println("success");
-				} finally {
-					instream.close();
+			HttpGet httpget = new HttpGet("http://192.168.3.135:8080/IoTWebProgramming/http/exam01");			
+			CloseableHttpResponse response = httpclient.execute(httpget);
+			try {
+				HttpEntity entity = response.getEntity();
+				if (entity != null) {
+					InputStream instream = entity.getContent();
+					try {
+						InputStreamReader isr = new InputStreamReader(instream);
+						BufferedReader br = new BufferedReader(isr);
+						String json = "";
+						while(true){
+							String data = br.readLine();
+							if(data == null) break;
+							json += data;
+						}
+						JSONObject jsonObject = new JSONObject(json);
+						String temperature = jsonObject.getString("temperature");
+						System.out.println(temperature);						
+					} catch (Exception e) {
+						e.printStackTrace();
+					} finally {
+						instream.close();
+					}
 				}
-			}
-		} finally {
-			response.close();
+			} finally {
+				response.close();
+			}			
+		} catch(Exception e){
+			e.printStackTrace();
+		}finally {
+			httpclient.close();
 		}
 	}
 }
