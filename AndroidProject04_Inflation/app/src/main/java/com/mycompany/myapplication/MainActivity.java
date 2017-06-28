ecaeebe4382ce0e3a12818cb3ce228410699af56
Kim YoungMin <happistar09@naver.com>
2017-06-28 17:45:48 +0900

    Android 0628

---
+++ AndroidProject04_Inflation/app/src/main/java/com/mycompany/myapplication/MainActivity.java
@@ -0,0 +1,57 @@
+package com.mycompany.myapplication;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+    private LinearLayout linearLayoutTop;
+    private FrameLayout frameLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        linearLayoutTop = (LinearLayout) findViewById(R.id.linearLayoutTop);
+        frameLayout = (FrameLayout) findViewById(R.id.frameLayout);
+
+
+    }
+
+    public void handleRadioButton1(View v) {
+        linearLayoutTop.setBackgroundResource(R.drawable.photo1);
+    }
+    public void handleRadioButton2(View v) {
+        linearLayoutTop.setBackgroundResource(R.drawable.photo2);
+    }
+    public void handleRadioButton3(View v) {
+        linearLayoutTop.setBackgroundResource(R.drawable.photo3);
+    }
+
+    public void handleButton1(View v) {
+        frameLayout.removeAllViews();
+        Content1 content1 = new Content1(this);
+        frameLayout.addView(content1);
+        for(int i=1; i<=10; i++) {
+            Item1 item = new Item1();
+            item.setPhoto(getResources().getIdentifier("member"+i,"drawable", getPackageName()));
+            item.setTitle("반복 학습이 중요" + i);
+            item.setStar(getResources().getIdentifier("star"+i,"drawable", getPackageName()));
+            item.setContent("이거 너무 쉬운거 아닙니까? 반복학습을 꼭 해야겠어요. 내 것으로 만들어야 겠어요.");
+            content1.addItem(item);
+        }
+    }
+    public void handleButton2(View v) {
+
+    }
+    public void handleButton3(View v) {
+
+    }
+
+}
